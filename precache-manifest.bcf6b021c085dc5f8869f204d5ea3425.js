2d88c8265707661240587563ef1615eb2e519b9e
unknown <a0984009426@gmail.com>
2020-08-25 11:51:32 +0800

    test-commit

---
+++ precache-manifest.bcf6b021c085dc5f8869f204d5ea3425.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "589a86bac87bad7d4b0a645a4fbff3e3",
+    "url": "/doshi-web-deploy/index.html"
+  },
+  {
+    "revision": "b17e1e4a484cc04e6758",
+    "url": "/doshi-web-deploy/static/css/main.bbd0b7ba.chunk.css"
+  },
+  {
+    "revision": "4e98977962d14dd7bbb2",
+    "url": "/doshi-web-deploy/static/js/2.327ad208.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/doshi-web-deploy/static/js/2.327ad208.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b17e1e4a484cc04e6758",
+    "url": "/doshi-web-deploy/static/js/main.c422a3c2.chunk.js"
+  },
+  {
+    "revision": "69734f7236c96dd1b041",
+    "url": "/doshi-web-deploy/static/js/runtime-main.d5800b2a.js"
+  },
+  {
+    "revision": "7bee2325012b6493c2a0d9f9090fb391",
+    "url": "/doshi-web-deploy/static/media/iconfont.7bee2325.svg"
+  },
+  {
+    "revision": "9e74cc5e1e463662ec4735a9988f9149",
+    "url": "/doshi-web-deploy/static/media/iconfont.9e74cc5e.woff"
+  },
+  {
+    "revision": "a5ad186daad00a37f535b48d81ddc08c",
+    "url": "/doshi-web-deploy/static/media/iconfont.a5ad186d.eot"
+  },
+  {
+    "revision": "d94bbf6aeb7375060d57063101cb98bb",
+    "url": "/doshi-web-deploy/static/media/iconfont.d94bbf6a.ttf"
+  },
+  {
+    "revision": "0abe28a8921085cfff0d91649e098e23",
+    "url": "/doshi-web-deploy/static/media/logo.0abe28a8.png"
+  },
+  {
+    "revision": "e18798ae6740ebd363c29ec96d16f905",
+    "url": "/doshi-web-deploy/static/media/logo_pure.e18798ae.png"
+  },
+  {
+    "revision": "31aab2e2b860315397d621ab0344ee5e",
+    "url": "/doshi-web-deploy/static/media/map.31aab2e2.png"
+  },
+  {
+    "revision": "aba9de2d23eff684aab3d2d4bbde5f30",
+    "url": "/doshi-web-deploy/static/media/more.aba9de2d.png"
+  },
+  {
+    "revision": "012bd65d1c14d3bf2b13ce12591c1d97",
+    "url": "/doshi-web-deploy/static/media/search.012bd65d.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.79c369988138a5746c0dbb7b950bee54.js"
+  "/doshi-web-deploy/precache-manifest.bcf6b021c085dc5f8869f204d5ea3425.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/doshi-web-deploy/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
