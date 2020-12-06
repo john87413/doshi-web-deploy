d1efd95f23d369e3e9ddbae455ae8bb7a65d34f1
unknown <a0984009426@gmail.com>
2020-12-06 21:52:12 +0800

    web deploy HA

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./doshi-web-deploy/precache-manifest.74f823ffafac7a33ec50111033c4267e.js"
+  "/doshi-web-deploy/precache-manifest.1f86a160869ddbe34303ff6742d51a3f.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./doshi-web-deploy/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/doshi-web-deploy/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
